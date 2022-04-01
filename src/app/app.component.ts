f64006e1434d15c75d94af71a92ad3c9d5ce4455
Sakshi2703 <43651038+Sakshi2703@users.noreply.github.com>
2022-04-01 16:14:15 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aha-app';
+  @Input() api;
+
+  constructor(private readonly router: Router) {
+    console.log(this.api);
+    
+    this.router.navigate(['dashboard',{state:this.api}]);
+
+
+
+  }
+
+
+  submit() {
+
+    if(sessionStorage.getItem("authenticated")==="true"){
+      var productKey = document.getElementById("product-key")["value"];
+      
+              // if (sessionStorage.getItem("buttonClicked") === "clicked") {
+                this.api["get"]("/products/" + sessionStorage.getItem("productKey") + "/features", {}, function (response) {
+                  // Do something with the features.
+                  console.log(response);
+                  this.api["put"]("/features/" + response["features"][0]["reference_num"], {
+                    "feature": {
+                      "name": "New name"
+                    }
+                  }, function (response) {
+                    // Do something with the features.
+                    console.log(response);
+                    
+                    sessionStorage.setItem("buttonClicked", "notclicked");
+                  })
+                 
+                });
+              // }
+            }
+
+  }
 }
