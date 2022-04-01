83aa5dddb1c7ae2f9f17f0b045e11ee639fa4600
Sakshi2703 <43651038+Sakshi2703@users.noreply.github.com>
2022-04-01 15:33:14 +0530

    Update dashboard.component.ts

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,13 +2,47 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+sessionStorage = sessionStorage;
+  constructor() { 
   }
 
-}
+  ngOnInit(): void {
+   
+        }
+
+        submit() {
+          document.getElementById("buttonsubmit").onclick = function () {
+            if (document.getElementById("product-key")["value"] !== "") {
+              sessionStorage.setItem("productKey", document.getElementById("product-key")["value"]);
+            }
+            sessionStorage.setItem("buttonClicked", "clicked");
+          }
+
+
+          if(sessionStorage.getItem("authenticated")==="true"){
+            var productKey = document.getElementById("product-key")["value"];
+            
+                    // if (sessionStorage.getItem("buttonClicked") === "clicked") {
+                      sessionStorage.getItem("api")["get"]("/products/" + sessionStorage.getItem("productKey") + "/features", {}, function (response) {
+                        // Do something with the features.
+                        console.log(response);
+                        sessionStorage.getItem("api")["put"]("/features/" + response["features"][0]["reference_num"], {
+                          "feature": {
+                            "name": "New name"
+                          }
+                        }, function (response) {
+                          // Do something with the features.
+                          console.log(response);
+                          
+                          sessionStorage.setItem("buttonClicked", "notclicked");
+                        })
+                       
+                      });
+                    // }
+                  }
+
+        }
+  }
